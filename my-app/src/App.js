e6b072e2905fa2bffde0809beb818ae6a4ae49b3
lumineaux <forouzmohammadi71@yahoo.com>
2022-05-09 02:36:06 +0430

    fix:correction routes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from 'redux/store'
-import { RoutesObj } from 'Routes/RoutesObj'
+import RouteApp from 'Routes/RoutesObj'
 
 function App() {
   return (
     <Provider store={store}>
-    <Router>
-      <Routes>
-        {RoutesObj.map((route) => {
-          return (
-            <Route
-              key={route.id}
-              path={route.pathname}
-              element={route.element}
-            />
-          )
-        })}
-      </Routes>
-    </Router>
+      <RouteApp />
     </Provider>
   )
 }
--- my-app/src/Routes/RoutesObj.js
+++ my-app/src/Routes/RoutesObj.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from 'pages/home/Home'
 import Basket from 'pages/basket/Basket'
 import Checkout from 'pages/checkout/Checkout'
@@ -9,15 +10,92 @@ import PaymentSuccess from 'pages/paymentSuccess/PaymentSuccess'
 import Products from 'pages/products/Products'
 import Product from 'pages/product/Product'
 
-export const RoutesObj =  [
-    {pathname: "/", name: "Home Page",isPrivate:false, id: "home", element: <Home/>},
-    {pathname: "/basket", name: "basket",isPrivate:false, id: "basket", element: <Basket/>},
-    {pathname: "/checkout", name: "checkout",isPrivate:false, id: "checkout", element: <Checkout/>},
-    {pathname: "/panelLogin", name: "Panel Login",isPrivate:true, id: "panelLogin", element: <PanelLogin/>},
-    {pathname: "/panelProduct", name: "Panel Product",isPrivate:false, id: "panelProduct", element: <PanelProduct/>},
-    {pathname: "/payment", name: "Payment",isPrivate:false, id: "payment", element: <Payment/>},
-    {pathname: "/paymentFail", name: "Payment Fail",isPrivate:false, id: "paymentFail", element: <PaymentFail/>},
-    {pathname: "/paymentSuccess", name: "Payment Success",isPrivate:false, id: "paymentSuccess", element: <PaymentSuccess/>},
-    {pathname: "/products", name: "Products",isPrivate:false, id: "Products", element: <Products/>},
-    {pathname: "/product", name: "Product",isPrivate:false, id: "Product", element: <Product/>},
-];
+const RoutesObj = [
+  {
+    pathname: '/',
+    name: 'Home Page',
+    isPrivate: false,
+    id: 'home',
+    element: <Home />,
+  },
+  {
+    pathname: '/basket',
+    name: 'basket',
+    isPrivate: false,
+    id: 'basket',
+    element: <Basket />,
+  },
+  {
+    pathname: '/checkout',
+    name: 'checkout',
+    isPrivate: false,
+    id: 'checkout',
+    element: <Checkout />,
+  },
+  {
+    pathname: '/panelLogin',
+    name: 'Panel Login',
+    isPrivate: true,
+    id: 'panelLogin',
+    element: <PanelLogin />,
+  },
+  {
+    pathname: '/panelProduct',
+    name: 'Panel Product',
+    isPrivate: false,
+    id: 'panelProduct',
+    element: <PanelProduct />,
+  },
+  {
+    pathname: '/payment',
+    name: 'Payment',
+    isPrivate: false,
+    id: 'payment',
+    element: <Payment />,
+  },
+  {
+    pathname: '/paymentFail',
+    name: 'Payment Fail',
+    isPrivate: false,
+    id: 'paymentFail',
+    element: <PaymentFail />,
+  },
+  {
+    pathname: '/paymentSuccess',
+    name: 'Payment Success',
+    isPrivate: false,
+    id: 'paymentSuccess',
+    element: <PaymentSuccess />,
+  },
+  {
+    pathname: '/products',
+    name: 'Products',
+    isPrivate: false,
+    id: 'Products',
+    element: <Products />,
+  },
+  {
+    pathname: '/product',
+    name: 'Product',
+    isPrivate: false,
+    id: 'Product',
+    element: <Product />,
+  },
+]
+export default function RouteApp() {
+  return (
+    <Router>
+      <Routes>
+        {RoutesObj.map((route) => {
+          return (
+            <Route
+              key={route.id}
+              path={route.pathname}
+              element={route.element}
+            />
+          )
+        })}
+      </Routes>
+    </Router>
+  )
+}
